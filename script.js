2ed43d3e3d9262a05cd18ec909fff9dadb83a272
Mobin <156933806+mobinrhmti@users.noreply.github.com>
2025-01-01 16:07:01 +0330

    Update script.js

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ console.log(document.querySelector('.guess').value);
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   let guess = Number(document.querySelector('.guess').value);
@@ -25,6 +26,11 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = '🎉 correct Number!';
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+    if (score>highscore){
+      highscore=score;
+      document.querySelector('.highscore').textContent=highscore
+      
+    }
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📈 too high!';
